7bb9f7ed11e0da426b3501fd50c1446a5e3ae26b
wenbo <1050794513@qq.com>
2020-06-01 20:36:09 +0800

    修复了支付宝沙箱状态不修改bug 优化了前端页面

---
+++ src/main/java/ltd/xx/mall/service/impl/XxMallIndexConfigServiceImpl.java
@@ -0,0 +1,99 @@
+package ltd.xx.mall.service.impl;
+
+import ltd.xx.mall.common.ServiceResultEnum;
+import ltd.xx.mall.controller.vo.XxMallIndexConfigGoodsVO;
+import ltd.xx.mall.dao.IndexConfigMapper;
+import ltd.xx.mall.dao.XxMallGoodsMapper;
+import ltd.xx.mall.entity.IndexConfig;
+import ltd.xx.mall.entity.XxMallGoods;
+import ltd.xx.mall.service.XxMallIndexConfigService;
+import ltd.xx.mall.util.BeanUtil;
+import ltd.xx.mall.util.PageQueryUtil;
+import ltd.xx.mall.util.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class XxMallIndexConfigServiceImpl implements XxMallIndexConfigService {
+
+    @Autowired
+    private IndexConfigMapper indexConfigMapper;
+
+    @Autowired
+    private XxMallGoodsMapper goodsMapper;
+
+    @Override
+    public PageResult getConfigsPage(PageQueryUtil pageUtil) {
+        List<IndexConfig> indexConfigs = indexConfigMapper.findIndexConfigList(pageUtil);
+        int total = indexConfigMapper.getTotalIndexConfigs(pageUtil);
+        PageResult pageResult = new PageResult(indexConfigs, total, pageUtil.getLimit(), pageUtil.getPage());
+        return pageResult;
+    }
+
+    @Override
+    public String saveIndexConfig(IndexConfig indexConfig) {
+        //todo 判断是否存在该商品
+        if (indexConfigMapper.insertSelective(indexConfig) > 0) {
+            return ServiceResultEnum.SUCCESS.getResult();
+        }
+        return ServiceResultEnum.DB_ERROR.getResult();
+    }
+
+    @Override
+    public String updateIndexConfig(IndexConfig indexConfig) {
+        //todo 判断是否存在该商品
+        IndexConfig temp = indexConfigMapper.selectByPrimaryKey(indexConfig.getConfigId());
+        if (temp == null) {
+            return ServiceResultEnum.DATA_NOT_EXIST.getResult();
+        }
+        if (indexConfigMapper.updateByPrimaryKeySelective(indexConfig) > 0) {
+            return ServiceResultEnum.SUCCESS.getResult();
+        }
+        return ServiceResultEnum.DB_ERROR.getResult();
+    }
+
+    @Override
+    public IndexConfig getIndexConfigById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<XxMallIndexConfigGoodsVO> getConfigGoodsesForIndex(int configType, int number) {
+        List<XxMallIndexConfigGoodsVO> xxMallIndexConfigGoodsVOS = new ArrayList<>(number);
+        List<IndexConfig> indexConfigs = indexConfigMapper.findIndexConfigsByTypeAndNum(configType, number);
+        if (!CollectionUtils.isEmpty(indexConfigs)) {
+            //取出所有的goodsId
+            List<Long> goodsIds = indexConfigs.stream().map(IndexConfig::getGoodsId).collect(Collectors.toList());
+            List<XxMallGoods> newBeeMallGoods = goodsMapper.selectByPrimaryKeys(goodsIds);
+            xxMallIndexConfigGoodsVOS = BeanUtil.copyList(newBeeMallGoods, XxMallIndexConfigGoodsVO.class);
+            for (XxMallIndexConfigGoodsVO xxMallIndexConfigGoodsVO : xxMallIndexConfigGoodsVOS) {
+                String goodsName = xxMallIndexConfigGoodsVO.getGoodsName();
+                String goodsIntro = xxMallIndexConfigGoodsVO.getGoodsIntro();
+                // 字符串过长导致文字超出的问题
+                if (goodsName.length() > 30) {
+                    goodsName = goodsName.substring(0, 30) + "...";
+                    xxMallIndexConfigGoodsVO.setGoodsName(goodsName);
+                }
+                if (goodsIntro.length() > 22) {
+                    goodsIntro = goodsIntro.substring(0, 22) + "...";
+                    xxMallIndexConfigGoodsVO.setGoodsIntro(goodsIntro);
+                }
+            }
+        }
+        return xxMallIndexConfigGoodsVOS;
+    }
+
+    @Override
+    public Boolean deleteBatch(Long[] ids) {
+        if (ids.length < 1) {
+            return false;
+        }
+        //删除数据
+        return indexConfigMapper.deleteBatch(ids) > 0;
+    }
+}
